a91495e2a9ff023bedfbf3ca11e11f345e44d502
t0k02w6 <tanaya.karmakar@walmartlabs.com>
2021-09-27 07:19:36 +0530

    ms-naming-server

---
+++ src/test/java/com/app/namingserver/MsNamingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.namingserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsNamingServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
